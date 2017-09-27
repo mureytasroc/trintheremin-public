5cd033d2ec70d1dd7d0da557bc25b72b78c68016
Charles Cunningham <mureytasroc@gmail.com>
2017-09-26 21:46:19 -0400

    Update TrinThereminExampleCode.ino

--- Example Code/TrinThereminExampleCode.ino
+++ Example Code/TrinThereminExampleCode.ino
@@ -44,11 +44,10 @@ byte scaleLength0=8; //Scale 0 Length
 float scale1[]={440,495,556.875,660,742.5}; //Pentatonic Scale
 byte scaleLength1=5; //Scale 1 Length
 
-float scale2[]={9.1770,11.5623,13.7500,18.3540,23.1247,30.8677,
-36.7081,46.2493,61.7354,73.4162,97.9989,123.4708,146.8324,
+float scale2[]={61.7354,73.4162,97.9989,123.4708,146.8324,
 195.9977,246.9417,329.6276,391.9954,493.8833,
 659.2551,783.9909}; //Pentatonic D Scale
-byte scaleLength2=20; //Scale 2 Length
+byte scaleLength2=12; //Scale 2 Length
 
 float scale3[]={196.00,220.00,246.94,261.63,293.66,
 329.63,369.99,392.00,440.00,493.88,523.25,587.33,659.25,
@@ -56,10 +55,11 @@ float scale3[]={196.00,220.00,246.94,261.63,293.66,
 byte scaleLength3=15; //Scale 3 Length
 
 //Other Variables
-unsigned long ts=0; //Counter to enact delays without breaking loop
-unsigned long ts2=0; //Counter to enact delays without breaking loop
-unsigned long avgReading=0; //Average sensor reading for calibration
-unsigned long counter=0; //Iteration counter for calibration
+unsigned long ts=0L; //Counter to enact delays without breaking loop
+unsigned long ts2=0L; //Counter to enact delays without breaking loop
+float avgReading=0; //Ambient sensor reading for calibration
+float lowAvg=0; //Average sensor reading when sensor is covered
+float counter=0; //Iteration counter for calibration
 boolean autoPlay=false; //Boolean autoplay toggle
 
 
@@ -77,45 +77,74 @@ void setup() {
 
   
   //Calibration-------------------------------vvv------------------
+  //Ambient Reading
   ts=millis(); //Start counter at current clock time
   ts2=millis(); //Start counter for alternating LEDs
-  while(millis()-ts<5000){ //While clock time - start time is < 5s
+  while(millis()-ts<3500){ //While clock time - start time is < 3.5s
     counter++; //Running counter of how many readings are taken
     avgReading+=analogRead(0); //Add current reading to avgReading
 
-    //Alternating blue/green LEDs to show calibration in progress
-    if(millis()-ts2>100){ //For the first 100 milliseconds:
-      analogWrite(blueLedPin,10); //Blue light on
-      analogWrite(greenLedPin,0); //Green light off
+    //Blue LED blinking to show ambient calibration in progress
+    if(millis()-ts2>400){ //For the first 400 milliseconds:
+      digitalWrite(blueLedPin,HIGH); //Blue light on
     }
-    if(millis()-ts2>200){ //For the second 100 milliseconds:
-      analogWrite(blueLedPin,0); //Blue LED off
-      analogWrite(greenLedPin,20); //Green LED on
+    if(millis()-ts2>800){ //For the second 400 milliseconds:
+      digitalWrite(blueLedPin,LOW); //Blue LED off
       ts2=millis();
     }
   //End of Calibration Light Display
-    
+
+  
   }
   avgReading/=counter;//set avgReading to the average sensor reading
+
+  digitalWrite(blueLedPin,LOW);
+  digitalWrite(greenLedPin,LOW); //Set both LEDs to LOW to tell the user to put their hand over the sensor
+  toneAC(500,4); //Play sound to tell user to put their hand over the sensor
+  delay(1500); //Gives the user time to put their hand on the sensor 
+  noToneAC();
+  
+  //Low Reading
+  ts=millis(); //Start counter at current clock time
+  ts2=millis(); //Start counter for alternating LEDs
+  counter=0; //Reset counter
+  while(millis()-ts<3500){ //While clock time - start time is < 3.5s
+    counter++; //Running counter of how many readings are taken
+    lowAvg+=analogRead(0); //Add current reading to lowAvg
+    
+    //Both LEDs blinking to show low level calibration in progress
+    if(millis()-ts2>400){ //For the first 400 milliseconds:
+      digitalWrite(blueLedPin,HIGH); //Blue light on
+      digitalWrite(greenLedPin,HIGH); //Green light on
+    }
+    if(millis()-ts2>800){ //For the second 400 milliseconds:
+      digitalWrite(blueLedPin,LOW); //Blue LED off
+      digitalWrite(greenLedPin,LOW); //Green LED off
+      ts2=millis();
+    }
+  //End of Calibration Light Display
+
+  }
+  lowAvg/=counter;//set avgLowReading to the average sensor reading
   //End of Calibration--------------------------^^^-----------------
 
 
   //Startup Light Sequence----------------------vvv-----------------
   digitalWrite(greenLedPin,LOW);  //All lights off
   digitalWrite(blueLedPin,LOW);   
-  delay(700);                     //Delay for 700ms
+  delay(200);                     //Delay for 200ms
   digitalWrite(greenLedPin,HIGH); //Green light on
-  delay(500);                     //Delay for 500ms
+  delay(100);                     //Delay for 100ms
   digitalWrite(blueLedPin,HIGH);  //Blue light on
-  delay(250);                     //Delay for 250ms
+  delay(100);                     //Delay for 100ms
   digitalWrite(greenLedPin,LOW);
   digitalWrite(blueLedPin,LOW);   //All lights off
-  delay(250);                     //Delay for 250ms
+  delay(100);                     //Delay for 100ms
   digitalWrite(greenLedPin,HIGH); 
   digitalWrite(blueLedPin,HIGH);  //All lights on
-  delay(500);                     //Delay for 500ms
+  delay(200);                     //Delay for 200ms
   //End Startup Light Sequence--------------------^^^----------------
-  }
+ }
 
 
 
@@ -135,8 +164,7 @@ void loop() {
 
 
   //Set current scale (depending on right slider)----------vvv------
-  if(digitalRead(switchPin)==HIGH){ //If the play switch is on
-    if(digitalRead(botButtonPin)==LOW){ //If the pause button is off
+  if((digitalRead(switchPin)==HIGH)&&(digitalRead(botButtonPin)==LOW)){ //If the play switch is on and the pause button is off
       int scaleSliderVal=analogRead(rightSliderPin);
       if(scaleSliderVal<250){
         sound(scale1,scaleLength1);
@@ -154,7 +182,7 @@ void loop() {
           } //c
         } //b
       } //a
-    }
+    
   }
   else{ //If play switch is off
     noToneAC(); //Turn off speaker
@@ -170,18 +198,17 @@ void loop() {
 
 //===========================SOUND FUNCTION=========================
 void sound(float thisScale[],int thisLength){
-   
-   int minVal=0; //Usual Minimum Light Sensor Reading (0-1023)
-   int maxVal=0; //Usual Maximum Light Sensor Reading (0-1023)
-   int closestPos=0; //The note position in the scale array (TBD)
-   double closestVal=10000000000;//vvv
+  float minVal=0; //Minimum light sensor value (determined below)
+  float maxVal=0; //Maximum light sensor value (determined below)
+  int closestPos=0; //The note position in the scale array (TBD)
+  double closestVal=10000000000;//vvv
    //^^^closest note value to light sensor reading (TBD)
    //^^^This is big so its initial value will never be the closest
 
 
   //Pick note-------------------vvv---------------------------------
   if(autoPlay==false){ //If autoplay is off
-    minVal=200; //Usual value when sensor is fully covered
+    minVal=lowAvg; //Usual value when sensor is fully covered
     maxVal=avgReading; //Reading cannot exceed ambient light level
     for(int i=0;i<thisLength;i++){
       float thisVal=thisScale[i]; //Save current position to thisVal
@@ -192,8 +219,8 @@ void sound(float thisScale[],int thisLength){
     }
   }
   else{ //If autoplay is on (notes optimized for autoplay)
-    minVal=600; //A range of 600-800 works best with the dancing LED
-    maxVal=800; //Ambient values not considered (dancing LED on)
+    minVal=lowAvg+500; //Dancing LED provides its own light so constants are added to the ambient light levels.
+    maxVal=avgReading+300; //These numbers were found to work best with the dancing LED.
     for(int i=0;i<thisLength;i++){
       float thisVal=thisScale[i];
       if(abs(map(analogRead(lightSensorPin),minVal,maxVal,thisScale[0],thisScale[thisLength-1])-thisVal)<closestVal){
@@ -204,7 +231,7 @@ void sound(float thisScale[],int thisLength){
   }
   //Note is Picked---------------^^^--------------------------------
 
-  int volVal=map(analogRead(leftSliderPin),0,1023,0,9.5);
+  int volVal=map(analogRead(leftSliderPin),0,1000,0,10); //The map HAS TO BE from 0 to 1000 or the sound quality will be bad at full volume
   //^^^ Sets volume (0-9.5 is a usable range)
   
   toneAC(thisScale[closestPos],volVal);
@@ -213,6 +240,5 @@ void sound(float thisScale[],int thisLength){
   
   analogWrite(blueLedPin,map(thisScale[closestPos],thisScale[0],thisScale[thisLength-1],0,255)); //overflow
   //^^^ Sets blue LED intensity proportional to the current note
-
 }
 //==================================================================
